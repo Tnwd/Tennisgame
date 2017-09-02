6cf2c631edb74379523c5a7042f9236207a9a4d3
Tanawadee Supraphakorn <58160395@go.buu.ac.th>
2017-09-02 16:11:43 +0700

    Passed test echo 'love - Fifteen'

--- tennis-game.spec.js
+++ tennis-game.spec.js
@@ -1,17 +1,22 @@
 function TennisGame() {
     const scoreString = ['Love', 'Fifteen', 'Thirty', 'Forthy']
     this.playerAScore = 0
+    this.playerBScore = 0
 
     this.reset = () => {
         this.playerAScore = 0
+        this.playerBScore = 0
     }
 
     this.echo = () => {
-       return `${scoreString[this.playerAScore]} - Love`
+       return `${scoreString[this.playerAScore]} - ${scoreString[this.playerBScore]}`
     }
 
-    this.playerAGteScore = () => {
+    this.playerAGetScore = () => {
         this.playerAScore++
+    }
+    this.playerBGetScore = () => {
+      this.playerBScore++
     }
 }
 
@@ -31,11 +36,24 @@ test('Echo "Fifteen - Love" when playerA get first score', () => {
     // Arrange
     let app = new TennisGame()
     app.reset()
-    app.playerAGteScore()
+    app.playerAGetScore()
 
     // Act
     let result = app.echo()
 
     // Assert
     expect(result).toBe('Fifteen - Love')
+})
+
+test('Echo "Love - Fifteen" when playerB get first score', () => {
+    // Arrange
+    let app = new TennisGame()
+    app.reset()
+    app.playerBGetScore()
+
+    // Act
+    let result = app.echo()
+
+    // Assert
+    expect(result).toBe('Love - Fifteen')
 })
